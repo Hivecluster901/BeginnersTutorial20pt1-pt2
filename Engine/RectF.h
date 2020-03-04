5e3eec24c5ee3bba2933ed64fc11cd5915230516
Minjun1234 <bill981212@gmail.com>
2020-03-04 21:12:29 +0900

    added a function that creates Rect from the center

--- Engine/RectF.h
+++ Engine/RectF.h
@@ -9,6 +9,7 @@ public:
 	RectF(const Vec2& topleft, const Vec2& bottomright);
 	RectF(const Vec2& topleft, float width, float height);
 	bool isOverLappingWith(const RectF& other) const;
+	static RectF FromCenter(const Vec2& center, float halfWidth, float halfHeight);
 public:
 	float left;
 	float right;
