beac2893eaaef32415c9cc6bdffb107e68839297
Minjun1234 <bill981212@gmail.com>
2020-03-05 02:14:03 +0900

    Brick Padding

--- Engine/RectF.h
+++ Engine/RectF.h
@@ -10,6 +10,7 @@ public:
 	RectF(const Vec2& topleft, float width, float height);
 	bool isOverLappingWith(const RectF& other) const;
 	static RectF FromCenter(const Vec2& center, float halfWidth, float halfHeight);
+	RectF GetExpanded(float offset) const;
 public:
 	float left;
 	float right;
