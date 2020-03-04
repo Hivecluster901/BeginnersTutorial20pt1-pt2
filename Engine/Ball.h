8857efdb3d8629e2703dcdd33fda18d80e3fc3c4
Minjun1234 <bill981212@gmail.com>
2020-03-04 21:12:48 +0900

    my balls are bouncing off the walls

--- Engine/Ball.h
+++ Engine/Ball.h
@@ -9,6 +9,10 @@ public:
 	Ball(const Vec2& pos, const Vec2& vel);
 	void Draw(Graphics& gfx) const;
 	void Update(float dt);
+	bool DoWallCollision(const RectF& walls);
+	void ReboundX();
+	void ReboundY();
+	RectF GetRect() const;
 private:
 	static constexpr float radius = 7.0f;
 	Vec2 pos;
