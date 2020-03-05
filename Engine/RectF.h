7d65e2a63b63384242cdfa640dbfd5feb009f16e
Minjun1234 <bill981212@gmail.com>
2020-03-06 01:22:28 +0900

    added the default constructor for RectF: this cleared the last error of "the default constructor cannot be referenced."

--- Engine/RectF.h
+++ Engine/RectF.h
@@ -3,6 +3,7 @@
 
 class RectF {
 public:
+	RectF() = default;
 	RectF(float left_in, float right_in, float top_in, float bottom_in);
 	RectF(const Vec2& topLeft, const Vec2& bottomRight);
 	RectF(const Vec2& topLeft, float width, float height);
