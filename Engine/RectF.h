0980618926c99ba7929c7f29decc10a35a7ff5bf
Minjun1234 <bill981212@gmail.com>
2020-03-06 20:04:34 +0900

    added GetExpanded function

--- Engine/RectF.h
+++ Engine/RectF.h
@@ -3,12 +3,13 @@
 
 class RectF {
 public:
-	//RectF() = default;
+	RectF() = default;
 	RectF(float left_in, float right_in, float top_in, float bottom_in);
 	RectF(const Vec2& topLeft, const Vec2& bottomRight);
 	RectF(const Vec2& topLeft, float width, float height);
 	bool IsOverlappingWith(const RectF& other) const;
 	static RectF FromCenter(const Vec2& center, float halfWidth, float halfHeight);
+	RectF GetExpanded(float padding) const;
 public:
 	float left;
 	float right;
